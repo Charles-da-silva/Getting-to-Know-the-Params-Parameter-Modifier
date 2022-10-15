946e4c737cdf367d84b30460c99683e5bb349877
Charles Silva <emaildocharles@gmail.com>
2022-10-15 12:25:16 -0300

    Getting to Know the Params Parameter Modifier

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿public class MyClass
+{
+    public static string UseParams(params int[] list)
+    {
+        int som = 0;
+        for (int i = 0; i < list.Length; i++)
+        {
+            som += list[i];
+        }
+
+        return "The som of the informed elements is: " + som.ToString();
+
+    }
+
+    public static void UseParams2(params object[] list)
+    {
+        for (int i = 0; i < list.Length; i++)
+        {
+            Console.Write(list[i] + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine("----------------------");
+    }
+
+    static void Main()
+    {
+        // You can send a comma-separated list of arguments of the
+        // specified type.
+        Console.WriteLine();
+        Console.WriteLine("----------------------");
+        Console.WriteLine(UseParams(4, 4));
+        Console.WriteLine("----------------------");
+        Console.WriteLine();
+        UseParams2(1, 'a', "test");
+       
+
+        // A params parameter accepts zero or more arguments.
+        // The following calling statement displays only a blank line.
+        UseParams2();
+
+        // An array argument can be passed, as long as the array
+        // type matches the parameter type of the method being called.
+        int[] myIntArray = { 5, 6, 7, 8, 9 };
+        UseParams(myIntArray);
+
+        object[] myObjArray = { 2, 'b', "test", "again" };
+        UseParams2(myObjArray);
+
+        // The following call causes a compiler error because the object
+        // array cannot be converted into an integer array.
+        //UseParams(myObjArray);
+
+        // The following call does not cause an error, but the entire
+        // integer array becomes the first element of the params array.
+        UseParams2(myIntArray);
+    }
+}
